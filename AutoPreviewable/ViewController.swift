6a7df23ecefde86643d2af3261ff97bb8df45a3d
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2019-09-21 01:46:41 +0900

    :new: Create new xcode project

---
+++ AutoPreviewable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoPreviewable
+//
+//  Created by AKIO on 2019/09/21.
+//  Copyright © 2019 AKIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
